fc65de351f25d17cc282f642233c2ec82d1da3ee
Jakeya French <jakeya.french@gmail.com>
2021-02-02 12:28:03 -0600

    feat: add js_html exercises

---
+++ JS/external.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Hello from external JavaScript");
+
+//alert
+alert("Welcome to my Website!");
+
+//Prompt
+var favoriteColor = prompt("What is your favorite color?");
+console.log('Great, ' + favoriteColor + ' is my favorite color too!');
+
+//prompt Question 3. previous exercise
+var movieRental =prompt("How many days did you rent '/The Little Mermaid'?")
+console.log('I rented ')
+
+
