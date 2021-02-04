cb351342d75e482f72577a7e752d98dcdafb86aa
Jakeya French <jakeya.french@gmail.com>
2021-02-04 14:34:19 -0600

    feat: JS/external.js exercise unfinished

--- JS/external.js
+++ JS/external.js
@@ -9,7 +9,19 @@ var favoriteColor = prompt("What is your favorite color?");
 console.log('Great, ' + favoriteColor + ' is my favorite color too!');
 
 //prompt Question 3. previous exercise
-var movieRental =prompt("How many days did you rent '/The Little Mermaid'?")
-console.log('I rented ')
+var LittleMermaid = parseInt(prompt("How many days have you rented Little Mermaid?"));
+var BrotherBear = parseInt(prompt("How many days have you rented BrotherBear?"));
+var Hercules = parseInt(prompt("How many days have you rented Hercules?"));
+var totalAmount = (LittleMermaid + BrotherBear + Hercules) * 3
+alert("The total due for your rentals is $" + totalAmount + "! Thank you for shopping with Jake's Movie Night!")
+
+var googleHours = Number(prompt("Please type your contracted hours here for Google."));
+var amazonHours = Number(prompt("Please type your contracted hours here for Amazon."));
+var facebookHours = Number(prompt("Please type your contracted hours here for Facebook."));
+var totalHoursWorked = parseInt((googleHours * 400) + (amazonHours * 380) + (facebookHours * 350));
+alert("Your total hours for all contracts is $" + totalHoursWorked)
+
+
+
 
 
