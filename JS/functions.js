7032460e18f09cec6beee6682596842f0f06aa94
Jakeya French <jakeya.french@gmail.com>
2021-02-28 20:11:15 -0600

    checking/correcting work

--- JS/functions.js
+++ JS/functions.js
@@ -1,20 +1,38 @@
 "use strict";
 
-//sayHello to do
-//var name = " Jake";
+// Create a function called 'sayHello' that takes a parameter 'name'.
+//   When called, the function should return a message that says hello to the passed in name.
+//  *
+//   Example
+//   sayHello("codeup") // returns "Hello, codeup!"
+//  To Do 1:
+
 function sayHello (name) {
     return ("Hello" + name +"!");
 }
-
-//second to do
 console.log(sayHello("Jake"));
+
 //console.log)sayHello(name));
 
-//helloMessage to o
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+var myName = "Jake";
+console.log(sayHello(myName));
+
+//helloMessage to do
 var helloMessage = (sayHello("Jake"));
 console.log(helloMessage);
 
-//myName to do
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
 var myName = "Jake";
 console.log(sayHello(myName));
 
